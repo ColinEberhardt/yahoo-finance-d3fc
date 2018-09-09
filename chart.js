150005c98ff5d5ccbef3628011ceaf8183447a2a
Colin Eberhardt <colin.eberhardt@gmail.com>
2018-09-09 21:33:20 +0100

    tidy

--- chart.js
+++ chart.js
@@ -20,6 +20,40 @@ const loadDataEndOfDay = d3.csv("/yahoo.csv", d => ({
   close: Number(d.close)
 }));
 
+
+const exchangeOpening = day => [
+  new Date(day.getFullYear(), day.getMonth(), day.getDate(), 9, 30, 0),
+  new Date(day.getFullYear(), day.getMonth(), day.getDate(), 16, 0, 0)
+];
+
+const tradingHours = dates => {
+  const getDateKey = date =>
+    date.getMonth() + "-" + date.getDate() + "-" + date.getFullYear();
+
+  const tradingHours = dates.reduce((acc, curr) => {
+    const dateKey = getDateKey(curr);
+    if (!acc.hasOwnProperty(dateKey)) {
+      acc[dateKey] = [curr, curr];
+    } else {
+      acc[dateKey][1] = curr;
+    }
+    return acc;
+  }, {});
+
+  return Object.keys(tradingHours).map(d => tradingHours[d]);
+};
+
+const closest = (arr, fn) =>
+  arr.reduce(
+    (acc, value, index) =>
+      fn(value) < acc.distance ? { distance: fn(value), index, value } : acc,
+    {
+      distance: Number.MAX_VALUE,
+      index: 0,
+      value: arr[0]
+    }
+  );
+
 const flatten = arr => [].concat.apply([], arr);
 
 const dateFormat = d3.timeFormat("%a %H:%M%p");
@@ -60,13 +94,13 @@ const gridlines = fc
 const annotation = fc
   .annotationSvgLine()
   .label(d => priceFormat(d))
-  .decorate((sel) =>
+  .decorate(sel =>
     sel
       .enter()
       .select(".right-handle")
       .append("g")
       .attr("transform", "translate(-40, 0)")
-      .call(callout());
+      .call(callout())
   );
 
 const verticalAnnotation = fc.annotationSvgLine().orient("vertical");
@@ -81,10 +115,6 @@ const chartLegend = legend();
 
 const crosshair = fc.annotationSvgCrosshair();
 
-const exchangeOpening = day => [
-  new Date(day.getFullYear(), day.getMonth(), day.getDate(), 9, 30, 0),
-  new Date(day.getFullYear(), day.getMonth(), day.getDate(), 16, 0, 0)
-];
 
 const multi = fc
   .seriesSvgMulti()
@@ -155,10 +185,8 @@ const chart = fc
   .yTicks(5)
   // https://github.com/d3/d3-axis/issues/32
   .yTickSize(40)
-  .yDecorate(sel => 
-    sel.select("text").attr("transform", "translate(10, -8)")
-  )
-  .xDecorate(sel => 
+  .yDecorate(sel => sel.select("text").attr("transform", "translate(10, -8)"))
+  .xDecorate(sel =>
     sel
       .select("text")
       .attr("dy", undefined)
@@ -166,33 +194,6 @@ const chart = fc
       .style("dominant-baseline", "central")
       .attr("transform", "translate(3, 10)")
   );
-
-const closest = (arr, fn) =>
-  arr.reduce(
-    (acc, value, index) =>
-      fn(value) < acc.distance ? { distance: fn(value), index, value } : acc,
-    {
-      distance: Number.MAX_VALUE,
-      index: 0,
-      value: arr[0]
-    }
-  );
-
-const getDateKey = date =>
-  date.getMonth() + "-" + date.getDate() + "-" + date.getFullYear();
-
-const tradingHours = dates => {
-  const tradingHours = dates.reduce((acc, curr) => {
-    const dateKey = getDateKey(curr);
-    if (!acc.hasOwnProperty(dateKey)) {
-      acc[dateKey] = [curr, curr];
-    } else {
-      acc[dateKey][1] = curr;
-    }
-    return acc;
-  }, {});
-  return Object.keys(tradingHours).map(d => tradingHours[d]);
-};
 
 loadDataIntraday.then(data => {
   data = data
